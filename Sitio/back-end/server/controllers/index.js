8786eab75ee8648cc85433443420f317df0e1e5d
Arturo Díaz Garza <artudiaz1999@gmail.com>
2023-05-17 00:31:21 -0600

    Front y Back

---
+++ Sitio/back-end/server/controllers/index.js
@@ -0,0 +1,7 @@
+const usuarios=require('./usuarios');
+const visitantes=require('./visitantes');
+
+module.exports={
+    usuarios,
+    visitantes
+};
---
+++ Sitio/back-end/server/controllers/usuarios.js
@@ -0,0 +1,105 @@
+const usuarios=require('../models').usuarios;
+const jwt=require('../services/jwt');
+
+function create(req,res){
+    usuarios.create(req.body)
+    .then(usuario=>{
+        res.status(200).send({usuario});
+    })
+    .catch(err=>{
+        res.status(500).send({message:'Error al crear el usuario',err});
+    });
+}
+
+function login(req,res){
+    usuarios.findOne({
+        where:{
+            usuario:req.body.usuario,
+            password:req.body.password
+        }
+    })
+    .then(usuario=>{
+        if(usuario){
+            if(req.body.token){
+                res.status(200).send({
+                    token:jwt.createToken(usuario)
+                });
+            }else{
+                res.status(200).send({
+                    usuario:usuario,
+                });
+            }
+        }else{
+            res.status(404).send({message:'Usuario o contraseña incorrectos'});
+        }
+    }).catch(err=>{
+        res.status(500).send({message:'Error al iniciar sesión',err});
+    });
+}
+
+function getAll(req,res){
+    usuarios.findAll({
+        order:[
+            ['id','ASC']
+        ]
+    })
+    .then(usuarios=>{
+        res.status(200).send({usuarios});
+    })
+    .catch(err=>{
+        res.status(500).send({message:'Error al obtener los usuarios.',err});
+    })
+}
+
+
+function deleteById(req,res){
+    var id=req.params.id;
+    usuarios.findOne ({
+        where:{
+            id:id
+        }
+    })
+    .then(usuario=>{
+        usuario.destroy()
+        .then(()=>{
+            res.status(200).send({usuario});
+        })
+        .catch(err=>{
+            res.status(500).send({message:'Error al eliminar el usuario',err});
+        });
+    })
+    .catch(err=>{
+        res.status(500).send({message:'Error al obtener el usuario',err});
+    });
+}
+
+function modifyById(req,res){
+    var id=req.params.id;
+    var body=req.body;
+    
+    usuarios.findOne ({
+        where:{
+            id:id
+        }
+    })
+    .then(usuario=>{
+        usuario.update(body)
+        .then(()=>{
+            res.status(200).send({usuario});
+        })
+        .catch(err=>{
+            res.status(500).send({message:'Error al actualizar el usuario',err});
+        });
+    })
+    .catch(err=>{
+        res.status(500).send({message:'Error al obtener el usuario',err});
+    });
+}
+
+module.exports={
+    create,
+    login,
+    getAll,
+    deleteById,
+    modifyById
+}
